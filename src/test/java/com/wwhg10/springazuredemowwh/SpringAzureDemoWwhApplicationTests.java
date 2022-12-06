7c806304b8bac9f3b60a9d893516595c3ef3fb07
Walter Hill <walter.hill@corteva.com>
2022-12-06 13:44:23 -0500

    first commit

---
+++ src/test/java/com/wwhg10/springazuredemowwh/SpringAzureDemoWwhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwhg10.springazuredemowwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoWwhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
